3d8b24f8932f7c495ddb9ecb03bfa2198ab735c5
TS.SZX.penglei <penglei@travelsky.com>
2019-11-13 17:04:05 +0800

    Initial commit

---
+++ src/test/java/com/mozaizai/databasehelper/DatabasehelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mozaizai.databasehelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabasehelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
